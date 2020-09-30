67e317146f6749c1fd9f4ba8fa857610d4d53d9a
Val <valeria.carvajal01@correo.usa.edu.co>
2020-09-30 10:43:35 -0500

    Funciones

--- functions.py
+++ functions.py
@@ -1,11 +1,27 @@
-import pandas as panda
+import pandas as pd
 import sqlite3 as bd
 
 #Conexion BD
-def connect(database){
+def conectar(database):
+    conn = None
+    try:
+        conn=bd.connect(database)
+        print("Se ha conectado a"+database)
+        conn.cursor()
+    except Exception as e:
+        print("Lo sentimos no se pudo conectar por"+e)
+        exit()
 
-}
-
-
+def terminarConexion(conn):
+    conn.commit()
+    conn.close()
 
 #Manejo informacion
+def extraerInformacion(url):
+    return pd.read_csv(url)
+
+def guardarInformacion(conn,data,tabla):
+    data.to_sql(tabla,conn,schema=None,if_exits:'replace',index=False,index_label=None)
+
+def consultar(conn,sentencia):
+    return pd.read_sql(sentencia,conn)
